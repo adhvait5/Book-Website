f7f2ae672551e33648aae0925cb635f4bc5cd2de
michellesun2 <122694710+michellesun2@users.noreply.github.com>
2023-08-21 13:43:06 -0700

    added to folder

---
+++ minesweeper/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+void startGame();
+
+int main{
+    startGame();
+    return 0;
+}
+
+void startGame(){
+    int numPlayers;
+    cout<<"Welcome to minesweeper!"<<endl;
+    cout<<"Please enter the number of players: ";
+    cin<<numPlayers;
+    cout << "Generating Board" << endl;
+}

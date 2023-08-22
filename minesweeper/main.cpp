29faa0c696bf4aedd470a66636436fc349ab3fe4
adhvait5 <adhvait2004@gmail.com>
2023-08-22 15:54:28 -0700

    random letter/number generator

--- minesweeper/main.cpp
+++ minesweeper/main.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
 void startGame();
 
-int main{
+int main(){
     startGame();
     return 0;
 }
 
 void startGame(){
     int numPlayers;
-    cout<<"Welcome to minesweeper!"<<endl;
-    cout<<"Please enter the number of players: ";
-    cin<<numPlayers;
+    cout << "Welcome to Uno!" << endl;
+    cout << "Please enter the number of players: ";
+    cin >> numPlayers;
     cout << "Generating Board" << endl;
+
+    int randomNumber = rand() % 10;
+    cout << random << endl;
+
+    char letters[] = "bgry";
+    char randomLetter = letters[rand() % 4];
+
+    cout << randomLetter << endl;
+
+    //string firstCard = "" + randomNumber + randomLetter; 
+
+    //cout << firstCard; 
+
+
+
+
 }
